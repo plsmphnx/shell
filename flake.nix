aafc986d26d96520b077707e77c5d8e2d11d9b2b
Carl LeCompte <clecompt@microsoft.com>
2026-01-06 12:32:31 -0800

    add module

--- flake.nix
+++ flake.nix
@@ -69,5 +69,31 @@
         ];
       };
     });
+
+    nixosModules.default = { config, lib, pkgs, ... }: let
+      cfg = config.programs.shell;
+      pkg = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    in with lib; {
+      options.programs.shell.enable = mkEnableOption "the shell";
+
+      config = mkIf cfg.enable {
+        environment = {
+          systemPackages = [ pkg ];
+          pathsToLink = [ "/share/shell" ];
+        };
+
+        systemd = {
+          packages = [ pkg ];
+          user.services.shell = {
+            environment.PATH = mkForce (concatStringsSep ":" [
+              "/run/wrappers/bin"
+              "/run/current-system/sw/bin"
+              "%h/.nix-profile/bin"
+            ]);
+            wantedBy = [ "graphical-session.target" ];
+          };
+        };
+      };
+    };
   };
 }
