5db02f4c0a487ef3b67450b95a11fdf654b1ac1a
Carl LeCompte <clecompt@microsoft.com>
2026-01-21 15:55:57 -0800

    support mask

--- lib/util/border.ts
+++ lib/util/border.ts
@@ -2,21 +2,28 @@ import { writeFile } from 'ags/file';
 
 import GLib from 'gi://GLib';
 
+const INDEX = [
+    [0, 0, 0, 0],
+    [0, 1, 1, 1],
+    [0, 0, 2, 2],
+    [0, 1, 1, 3],
+];
+
 export function build(radius: string, width: string, mask: string) {
     const r = Number(radius);
     const w = Number(width);
-    const s = 3 * (r + w);
+    const s = 2 * (r + w) + (r + 2 * w);
+    const m = INDEX.map(m => mask[m[mask.length - 1]] === '1');
     const d = [
         `M ${r + w} ${w / 2}`,
-        `l ${r + w} 0`,
-        `a 5 5 0 0 1 ${r + w / 2} ${r + w / 2}`,
-        `l 0 ${r + w}`,
-        `a 5 5 0 0 1 -${r + w / 2} ${r + w / 2}`,
-        `l -${r + w} 0`,
-        `a 5 5 0 0 1 -${r + w / 2} -${r + w / 2}`,
-        `l 0 -${r + w}`,
-        `a 5 5 0 0 1 ${r + w / 2} -${r + w / 2}`,
-        `z`,
+        `${m[0] ? 'l' : 'm'} ${r + 2 * w} 0`,
+        `${m[0] || m[1] ? 'a 5 5 0 0 1' : 'm'} ${r + w / 2} ${r + w / 2}`,
+        `${m[1] ? 'l' : 'm'} 0 ${r + 2 * w}`,
+        `${m[1] || m[2] ? 'a 5 5 0 0 1' : 'm'} -${r + w / 2} ${r + w / 2}`,
+        `${m[2] ? 'l' : 'm'} -${r + 2 * w} 0`,
+        `${m[2] || m[3] ? 'a 5 5 0 0 1' : 'm'} -${r + w / 2} -${r + w / 2}`,
+        `${m[3] ? 'l' : 'm'} 0 -${r + 2 * w}`,
+        `${m[3] || m[0] ? 'a 5 5 0 0 1' : 'm'} ${r + w / 2} -${r + w / 2}`,
     ].join(' ');
 
     const root = `${GLib.get_user_runtime_dir()}/shell`;
@@ -31,7 +38,7 @@ export function build(radius: string, width: string, mask: string) {
         `<svg ${svg}><path ${path} d="${d}" opacity="0.8"/></svg>`,
     );
 
-    const slice = String(r + w);
+    const slice = String(r + 2 * w);
 
     return {
         image0: `url("file://${root}/border.svg")`,
